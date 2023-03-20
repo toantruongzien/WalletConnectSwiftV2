a57cf91bec7c15958a415db09e7cbb64efc6d218
toantruongzien <111116856+toantruongzien@users.noreply.github.com>
2023-03-20 15:53:36 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "WalletConnect",
+            name: "WalletConnectV2",
             targets: ["WalletConnectSign"]),
         .library(
             name: "WalletConnectChat",
